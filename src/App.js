f468cccd6d4b5723302808e4970a8750afcb1e0a
Shashank Kumbar <shashankkumbar98@gmail.com>
2022-07-23 21:21:30 +0530

    Changed title

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ const App = () => {
   };
   return (
     <div className="App">
-      <h1 className="app-title">Monsters Rolodex</h1>
+      <h1 className="app-title">Monsters Rolodexes</h1>
       <SearchBox
         className="monsters-search-box"
         onChangeHandler={onSearchChange}
